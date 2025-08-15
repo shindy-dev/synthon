dd001ff1f9ea75a5c6e10140fea352e0620cd24c
shindy-dev <shindy.developer@gmail.com>
2025-08-15 22:13:57 +0900

    update

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""Minimal setup file for tasks project."""
+
+from setuptools import find_packages, setup
+
+setup(
+    name="synthon",
+    version="0.0.1",
+    license="proprietary",
+    description="Module Experiment",
+    author="shindy-dev",
+    author_email="shindy.developer@gmail.com",
+    url="shindy-dev.github.io",
+    packages=find_packages(where="."),
+)
